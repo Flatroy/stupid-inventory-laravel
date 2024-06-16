dc80f0ef0178178e44b9b53a7d25f845a97d741a
George Daneke <flatroy@gmail.com>
2024-06-16 22:57:04 +0300

    added filamentphp

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
 ];
