862207cbc64131d5ddd0ee5e0402d065ad9f5895
George Daneke <flatroy@gmail.com>
2024-06-26 00:16:07 +0300

    added QR code and import/export actions to recently added items widget

--- app/Filament/Widgets/RecentlyAdded.php
+++ app/Filament/Widgets/RecentlyAdded.php
@@ -2,9 +2,17 @@
 
 namespace App\Filament\Widgets;
 
+use App\Filament\Exports\ItemExporter;
+use App\Filament\Imports\ItemImporter;
 use App\Filament\Resources\ItemResource;
 use App\Models\Item;
+use Filament\Facades\Filament;
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Support\Enums\MaxWidth;
 use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Actions\ExportAction;
+use Filament\Tables\Actions\ImportAction;
 use Filament\Tables\Columns\TextInputColumn;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
@@ -34,8 +42,30 @@ class RecentlyAdded extends BaseWidget
                 Tables\Columns\CheckboxColumn::make('insured')->toggleable(),
             ])
             ->actions([
+
+                Action::make('ShowQrCode')
+                    ->iconButton()
+                    ->icon('heroicon-o-qr-code')
+                    ->modalHeading('QR Code:')
+                    ->action(function () {})
+                    ->label('Show QR Code')
+                    ->modalSubmitAction(false)
+                    ->modalWidth(MaxWidth::Small)
+                    ->modalCancelAction(fn ($action) => $action->label('Close'))
+                    ->infolist([
+                        ImageEntry::make('qr_code_url')->size(300)->label(''),
+                    ]),
+
                 Tables\Actions\Action::make('open')
+                    ->icon('heroicon-m-pencil-square')
                     ->url(fn (Item $record): string => ItemResource::getUrl('edit', ['record' => $record])),
+            ])
+            ->headerActions([
+                ImportAction::make()
+                    ->importer(ItemImporter::class)->options([
+                        'team_id' => Filament::getTenant()?->id,
+                    ]),
+                ExportAction::make()->exporter(ItemExporter::class),
             ]);
     }
 }
