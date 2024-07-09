7f305e3eddbbd78cd105afb0819c26f682b30e18
George Daneke <flatroy@gmail.com>
2024-07-09 11:54:59 +0300

    fixed QR code link to find user team by default

--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,25 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    /*Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');*/
     Route::redirect('/dashboard', '/app')->name('dashboard');
 });
-//asset_id
+
 Route::get('/a/{asset_id}', function (string $asset_id) {
-    $asset = \App\Models\Item::where('asset_id', $asset_id)->first();
+    $team_id = auth()?->user()?->currentTeam()?->getChild()?->id;
+    // todo: put this logic in one place
+    $asset_id = ltrim($asset_id, '0');
+    $asset_id = ltrim($asset_id, '-');
+
+    $asset = \App\Models\Item::where('asset_id', $asset_id)
+        ->where('team_id', $team_id)
+        ->first();
     if (! $asset) {
         // create new empty one with this asset id
         $asset = new \App\Models\Item();
         $asset->asset_id = $asset_id;
-        $asset->team_id = auth()?->user()?->currentTeam()?->getChild()?->id;
+        $asset->team_id = $team_id;
         $asset->save();
     }
 
-    return redirect()->to(ItemResource::getUrl('edit', ['record' => $asset]));
+    return redirect()->to(ItemResource::getUrl('edit', ['record' => $asset, 'tenant' => $team_id]));
 })->name('asset');
